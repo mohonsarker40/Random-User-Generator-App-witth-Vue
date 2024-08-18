363b36d6cd32c176d4ec7e537cd6d661facb6582
mohon sarker <mohonsarker40@gmail.com>
2024-08-18 12:46:02 +0600

    static data added without fetching

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const app = Vue.createApp({
     data (){
         return {
             firstName : 'Mohon',
+            lastName : 'Sarker',
+            email: 'morshedmohon4@gmail.com',
+            gender: 'male',
+            picture: 'https://randomuser.me/api/portraits/med/women/18.jpg',
         }
     }
 })
