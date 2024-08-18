5eee080698a120dce8e30417a11d474dee2b42a3
mohon sarker <mohonsarker40@gmail.com>
2024-08-18 14:31:43 +0600

    get a random static user with data fetch

--- app.js
+++ app.js
@@ -5,9 +5,24 @@ const app = Vue.createApp({
             lastName : 'Sarker',
             email: 'morshedmohon4@gmail.com',
             gender: 'male',
-            picture: 'https://randomuser.me/api/portraits/med/women/18.jpg',
+            picture: 'https://randomuser.me/api/portraits/med/men/18.jpg',
+        }
+    },
+    methods: {
+        async getUser (){
+            const res = await fetch('https://randomuser.me/api')
+            const { results } = await res.json()
+
+            console.log(results)
+
+            this.firstName = 'Voronika'
+            this.lastName = 'Clark'
+            this.email = 'voronika@gmail.com'
+            this.gender = 'female'
+            this.picture = 'https://randomuser.me/api/portraits/med/women/18.jpg'
         }
     }
+
 })
 
 app.mount('#app')
